466a38fd53b03785da091415b650942f80bf3394
jack <e.kobets@talantarium.com>
2017-06-14 19:29:36 +0300

    Some examples and documentation.

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -18,7 +18,7 @@ export class Hello extends React.Component<Props, HelloState> {
         this.state = {//заполнение полей стейта из пропсов
             name: props.lala,//заполнение полей стейта из пропсов, тут name заполняется из пропсов lala
             // name: null
-            lastName : 'lastName'
+            lastName: 'lastName'
         }
     }
 
@@ -61,7 +61,18 @@ export class Hello extends React.Component<Props, HelloState> {
              *  ({name: (e.target as HTMLInputElement).value})
              *  в name будет записываться значение из value тега <Input/>
              *
-             * Синтаксис на примере onBlur, обернутый в том же <Input/>, что и предыдущий onChange
+             * Синтаксис на примере onBlur, обернутый в том же <Input/>, что и предыдущий onChange:
+             <input
+             onChange={ e => this.setState({name: (e.target as HTMLInputElement).value}) }
+             onBlur={ () => {
+                        console.log('sss');
+                        this.triggerValidation('name')
+                    } }
+             />
+             означает, что при смене фокусас поля input будет вызван метод triggerValidation(fieldName),
+             в который в качестве параметра будет передано наше  в виде строки т.к. аргумент метода
+             не типизирован. Далее этот 'name' будет проверен на пксое значение и если так, то
+             в поле стейта fieldName, которое name запишется 'triggerValidation worked'
              */
             <div>
                 <h1>Hello, { this.state.name } !</h1>
