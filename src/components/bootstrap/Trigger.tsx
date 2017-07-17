19c902dda85260e2167678137d89f06d738ff027
jack <e.kobets@talantarium.com>
2017-07-17 19:30:14 +0300

    Fixed invisible modal text. Refactored.

--- src/components/bootstrap/Trigger.tsx
+++ src/components/bootstrap/Trigger.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Button, Modal} from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 
 /*Added Styles for modal tests*/
 require('../../styles/modal.css');
@@ -17,18 +17,18 @@ require('../../styles/modal.css');
 
 export const Trigger = React.createClass({
     getInitialState() {
-        return {show: false};
+        return { show: false };
     },
 
     render() {
-        let close = () => this.setState({show: false});
+        let close = () => this.setState({ show: false });
 
         return (
-            <div className="modal-container" style={{height: 200}}>
+            <div className="modal-container" style={{ height: 200 }}>
                 <Button
                     bsStyle="primary"
                     bsSize="large"
-                    onClick={() => this.setState({show: true})}
+                    onClick={ () => this.setState({ show: true }) }
                 >
                     Launch contained modal
                 </Button>
